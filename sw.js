c971f8e6a25f060e05c5bb7aec049f888fb927f1
abdo choufani <abdo.elchoufani@gmai.com>
2019-01-08 16:58:51 +0200

    adding ServiceWorker

--- sw.js
+++ sw.js
@@ -20,3 +20,41 @@ let urlsToCache = [
   './img/9.jpg',
   './img/10.jpg',
 ];
+
+
+/**Instaling service worker */
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache) {
+			return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+
+/**Activating service worker */
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+
+/**Offline first */
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
